cbd9c9dfc9a2078c184ac732e10f63ea0b25ef7f
dhsheng <duanhaisheng@gmail.com>
2022-04-17 11:16:15 +0800

    add typealias

--- Sources/Rivia/Rivia.swift
+++ Sources/Rivia/Rivia.swift
@@ -1,6 +1,27 @@
-public struct Rivia {
-    public private(set) var text = "Hello, World!"
+//AccountServiceClient
 
-    public init() {
-    }
-}
+import Security
+
+
+typealias RiviaAccountServiceClient = AccountServiceClient
+typealias RiviaAccountLoginWithOAuthRequest = LoginWithOAuthRequest
+typealias RiviaAccountLoginWithOAuthResponse = LoginResponse
+
+typealias RiviaEventServiceClient = EventServiceClient
+typealias RiviaPublishUserEventRequest = PublishUserEventRequest
+typealias RiviaPublishUserEventResponse = PublishUserEventResponse
+
+
+typealias RiviaUserServiceClient = UserServiceClient
+typealias RiviaFetchUserRequest = FetchUserRequest
+typealias RiviaFetchUserResponse = FetchUserResponse
+
+typealias RiviaFriendshipServiceClient = FriendshipServiceClient
+typealias RiviaFriendshipFetchFriendsRequest = FetchFriendsRequest
+typealias RiviaFriendshipFetchFriendsResponse = FetchFriendsResponse
+typealias RiviaFriendshipFetchFollwersRequest = FetchFollowersRequest
+typealias RiviaFriendshipFetchFollwersResponse = FetchFollowersResponse
+
+
+typealias RiviaIMConfigServiceClient = IMConfigServiceClient
+typealias RiviaIMFetchConfigRequest = FetchIMConfigRequest
